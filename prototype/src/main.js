720bf467d1276dc4548d41570eae68947d0b5255
Shawn Fan <sf19@rice.edu>
2019-12-09 12:54:27 -0600

    Vuetify installation

--- prototype/src/main.js
+++ prototype/src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import Router from './router.js'
 import VueKonva from 'vue-konva'
+import vuetify from './plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
@@ -10,5 +13,6 @@ Vue.use(VueKonva)
 new Vue({
   router: Router,
   template: "<App/>",
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
