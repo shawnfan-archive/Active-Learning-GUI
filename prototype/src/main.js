dc95a7a610654b36ac1fbab22932098ff699f605
Shawn Fan <sf19@rice.edu>
2019-12-09 12:57:08 -0600

    Revert "Vuetify installation"
    This reverts commit 720bf467d1276dc4548d41570eae68947d0b5255.
    
    undo Vuetify instllation

--- prototype/src/main.js
+++ prototype/src/main.js
@@ -2,9 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import Router from './router.js'
 import VueKonva from 'vue-konva'
-import vuetify from './plugins/vuetify';
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
@@ -13,6 +10,5 @@ Vue.use(VueKonva)
 new Vue({
   router: Router,
   template: "<App/>",
-  vuetify,
   render: h => h(App)
 }).$mount('#app')
